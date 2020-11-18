cd5906b3e801eaf972c8b5549acf79adaa288621
coollce <54264798+coollce@users.noreply.github.com>
2020-11-18 13:31:40 +0800

    Update and rename rad_xray to rad_xray.py

--- rad_xray.py
+++ rad_xray.py
@@ -1,21 +1,18 @@
 import subprocess,time
+#rad路径,使用相对路径
 rad_path="rad_windows_amd64.exe"
+#xray路径，使用相对路径
 xray_path="xray_windows_amd64.exe"
+#扫描的目标url
 url="http://10.51.30.34:8999"
+#xray启动代理地址
 xrayProxy="127.0.0.1:7799"
-# xrayShell = [xray_path, "webscan", "--listen", xrayProxy, "--html-output", "result.html"]
-# subprocess.Popen(xrayShell)
-#
-# cmd = [rad_path,"-t",url,"-http-proxy",xrayProxy]
-# rsp=subprocess.Popen(cmd,start_new_session=True)
-# rsp.communicate()
 
 def Scan(target,startxray):
-	cmd = [rad_path,"-t",target,"-wait-login","-http-proxy",xrayProxy]
+	cmd = [rad_path,"-t",target,"-http-proxy",xrayProxy]
 	rsp=subprocess.Popen(cmd,start_new_session=True)
 	rsp.communicate()
-
-
+	
 # with open("target.txt","r") as targats:
 xrayShell = [xray_path, "webscan", "--listen", xrayProxy, "--html-output", "result4.html"]
 startxray = subprocess.Popen(xrayShell)
